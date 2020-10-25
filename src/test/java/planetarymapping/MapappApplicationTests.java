ca286471654096bdb8364aac02ae816e513d7f60
Greg <gac5206@psu.edu>
2020-10-25 19:37:39 -0400

    CSS and backgrounds

--- src/test/java/planetarymapping/MapappApplicationTests.java
+++ src/test/java/planetarymapping/MapappApplicationTests.java
@@ -2,12 +2,35 @@ package planetarymapping;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import planetarymapping.controller.UserController;
 
 @SpringBootTest
 class MapappApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testCorrectLogin(){
+
+
+    }
+
+    @Test
+    void testBadLogin(){
+
+    }
+
+    @Test
+    void testGoodRegister(){
+
+
+    }
+
+    @Test
+    void testBadRegister(){
+
     }
 
 }
