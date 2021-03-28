3bffa091489c7000c216d8e2d25c9ab10c4a54b6
James Lanham <jrl5748@psu.edu>
2021-03-28 15:15:12 -0400

    Map admin pages finsihed

--- src/main/java/planetarymapping/controller/AdminController.java
+++ src/main/java/planetarymapping/controller/AdminController.java
@@ -91,67 +91,68 @@ public class AdminController {
     }
 
     /*Map Admin*/
-//    @GetMapping("/map")
-//    public String map(Model model){
-//        List<Map> maps = mapRepo.findAll();
-//        model.addAttribute("maps", maps);
-//        return "admin/map/admin-map";
-//    }
-//
-//    @GetMapping("/map/add")
-//    public String mapAdd(){
-//        return "admin/map/admin-map-add";
-//    }
-//
-//    @PostMapping("/map/add")
-//    public String mapAdded(Model model, @RequestParam("title") String title,
-//                              @RequestParam("caption") String caption, @RequestParam("image") String image){
-//
-//        Slide slide = new Slide(title, caption, image);
-//        slideRepo.save(slide);
-//
-//        List<Slide> slides = slideRepo.findAll();
-//        model.addAttribute("slides", slides);
-//        return "admin/slider/admin-slider";
-//    }
-//
-//    @GetMapping("/map/edit/{id}")
-//    public String mapEdit(@PathVariable int id, Model model){
-//
-//        Slide slide = slideRepo.findAllById(id);
-//        slideRepo.deleteById(id);
-//        model.addAttribute("slide", slide);
-//        return "admin/slider/admin-slider-edit";
-//    }
-//
-//    @PostMapping("/map/edit/{id}")
-//    public String mapEdited(Model model,@PathVariable int id, @RequestParam("title") String title,
-//                               @RequestParam("caption") String caption, @RequestParam("image") String image){
-//
-//        Slide slide = new Slide(id, title, caption, image);
-//
-//        slideRepo.save(slide);
-//
-//        List<Slide> slides = slideRepo.findAll();
-//        model.addAttribute("slides", slides);
-//        return "admin/slider/admin-slider";
-//    }
-//
-//    @GetMapping("/map/delete/{id}")
-//    public String mapDelete(@PathVariable int id, Model model){
-//
-//        Slide slide = slideRepo.findAllById(id);
-//        model.addAttribute("slide", slide);
-//        return "admin/slider/admin-slider-delete";
-//    }
-//
-//    @PostMapping("/map/delete/{id}")
-//    public String mapDeleted(Model model,@PathVariable int id){
-//
-//        slideRepo.deleteById(id);
-//
-//        List<Slide> slides = slideRepo.findAll();
-//        model.addAttribute("slides", slides);
-//        return "admin/slider/admin-slider";
-//    }
+    @GetMapping("/map")
+    public String map(Model model){
+        List<Map> maps = mapRepo.findAll();
+        model.addAttribute("maps", maps);
+        return "admin/map/admin-map";
+    }
+
+    @GetMapping("/map/add")
+    public String mapAdd(){
+        return "admin/map/admin-map-add";
+    }
+
+    @PostMapping("/map/add")
+    public String mapAdded(Model model, @RequestParam("title") String title,
+                              @RequestParam("link") String link, @RequestParam("facts") String facts,
+                           @RequestParam("slider_name") String slider_name, @RequestParam("image") String image){
+
+        Map map = new Map(title, link, facts, slider_name, image);
+        mapRepo.save(map);
+
+        List<Map> maps = mapRepo.findAll();
+        model.addAttribute("maps", maps);
+        return "admin/map/admin-map";
+    }
+
+    @GetMapping("/map/edit/{id}")
+    public String mapEdit(@PathVariable int id, Model model){
+
+        Map map = mapRepo.findAllById(id);
+        mapRepo.deleteById(id);
+        model.addAttribute("map", map);
+        return "admin/map/admin-map-edit";
+    }
+
+    @PostMapping("/map/edit/{id}")
+    public String mapEdited(Model model, @PathVariable int id, @RequestParam("title") String title,
+                            @RequestParam("link") String link, @RequestParam("facts") String facts,
+                            @RequestParam("sliderName") String slider_name, @RequestParam("image") String image){
+
+        Map map = new Map(id, title, link, facts, slider_name, image);
+        mapRepo.save(map);
+
+        List<Map> maps = mapRepo.findAll();
+        model.addAttribute("maps", maps);
+        return "admin/map/admin-map";
+    }
+
+    @GetMapping("/map/delete/{id}")
+    public String mapDelete(@PathVariable int id, Model model){
+
+        Map map = mapRepo.findAllById(id);
+        model.addAttribute("map", map);
+        return "admin/map/admin-map-delete";
+    }
+
+    @PostMapping("/map/delete/{id}")
+    public String mapDeleted(Model model,@PathVariable int id){
+
+        mapRepo.deleteById(id);
+
+        List<Map> maps = mapRepo.findAll();
+        model.addAttribute("maps", maps);
+        return "admin/map/admin-map";
+    }
 }
--- src/main/java/planetarymapping/model/Map.java
+++ src/main/java/planetarymapping/model/Map.java
@@ -21,6 +21,28 @@ public class Map {
 
     private String image;
 
+    public Map(){
+
+    }
+
+    public Map(String title, String link, String facts, String sliderName, String image){
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.facts = facts;
+        this.sliderName = sliderName;
+        this.image = image;
+    }
+
+    public Map(Integer id, String title, String link, String facts, String sliderName, String image){
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.facts = facts;
+        this.sliderName = sliderName;
+        this.image = image;
+    }
+
     public Integer getId(){
         return id;
     }
