27b228a70b9fac48bf6d1808de50aa3e362c72fe
James Lanham <jrl5748@psu.edu>
2021-03-30 19:40:33 -0400

    File upload for Slider

---
+++ src/main/java/planetarymapping/Service/FileService.java
@@ -0,0 +1,46 @@
+package planetarymapping.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileService {
+
+    @Value("${app.upload.dir:${user.home}}")
+    public String uploadDir;
+
+    public void uploadFile(MultipartFile file) {
+
+        try {
+            Path copyLocation = Paths
+                    .get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new FileStorageException("Could not store file " + file.getOriginalFilename()
+                    + ". Please try again!");
+        }
+    }
+}
+
+class FileStorageException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    private String msg;
+
+    public FileStorageException(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
